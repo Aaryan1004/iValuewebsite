a8eb7a1029c1c85af07e54ca7a71f7c4fa718ede
Aaryan1004 <aaryanagarwal_ee22b14_68@dtu.ac.in>
2026-06-21 16:25:11 +0530

    robots.ts changed

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { ObjectConfig } from '@/content/siteConfig';
 
+export const dynamic = 'force-static'; 
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
